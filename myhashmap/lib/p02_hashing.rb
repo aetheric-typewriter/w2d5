a868564298f27399f31236b19c39ae569b710402
syd <sfa95@icloud.com>
2019-04-19 14:37:02 -0400

    Work on hashing functions

--- myhashmap/lib/p02_hashing.rb
+++ myhashmap/lib/p02_hashing.rb
@@ -4,11 +4,20 @@ end
 
 class Array
   def hash
+	num = 0
+	self.each_with_index do |el, i|
+		num += el.object_id * (10 ** i)
+	end
+	num.hash
   end
 end
 
 class String
   def hash
+	chars = self.split("")
+	alpha = ("a".."z").to_a
+	nums = chars.map { |c| alpha.index(c) }
+	nums.hash
   end
 end
 
@@ -16,6 +25,11 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+	0
+	arr = []
+	self.each do |k, v|
+		arr << [k, v]
+	end
+	arr.sort.hash
   end
 end
